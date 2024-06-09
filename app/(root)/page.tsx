88b15ed642a0f01719eec0c860449e9b6de97a6d
Varun1802 <60618498+Varun1802@users.noreply.github.com>
2024-06-09 15:16:19 +0530

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,6 @@ const Home = () => {
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Process</h1>
-        <Button className="text-white-1 bg-orange-1">Button</Button>
       </section>
     </div>
   );
