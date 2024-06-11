6c7c88a38f8eac85edb68622495c399ced13d91a
Varun1802 <60618498+Varun1802@users.noreply.github.com>
2024-06-11 18:47:53 +0530

    imageadded

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
 import React from "react";
 
-const PodcastCard = () => {
-  return <div>PodcastCard</div>;
+const PodcastCard = ({
+  imgURL,
+  title,
+  description,
+  podcastId,
+}: {
+  imgURL: string;
+  title: string;
+  description: string;
+  podcastId: number;
+}) => {
+  return (
+    <div className="cursor-point">
+      <figure className="flex flex-col gap-2">
+        <Image src={imgURL} width={174} height={174} alt={title} />
+        <div>
+          <h1>{title}</h1>
+        </div>
+      </figure>
+    </div>
+  );
 };
 
 export default PodcastCard;
